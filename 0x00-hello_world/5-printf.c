4a0e085c5f61f89131ed76053f1fa92264966636
omar kamel <Omarkamel7.com>
2023-02-17 00:32:48 -0800

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-int main(void)
 /**
- * this code display on the screen.
+ *main-this code display on the screen.
+ *return ok
  */
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
