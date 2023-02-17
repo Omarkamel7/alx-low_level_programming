d4e00663495edbfc6313250eb0785009ed0649a6
omar kamel <Omarkamel7.com>
2023-02-16 23:59:45 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-int main(voidu)
+int main<void>
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 /**
- *this code have main function
+ * this function will sucsses.
  */
 }
