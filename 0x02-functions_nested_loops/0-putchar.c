53ace1e5aaf213254e700c430d67a1dd331570f7
omar kamel <Omarkamel7.com>
2023-02-22 10:50:02 -0800

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * descreption:Write a program that prints _putchar, followed by a new line.
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+char text[8] = "_putchar";
+for (int i = 0,i < 8,i++)
+{
+_putchar(text[i]);
+}
+_putchar("\n");
+}
