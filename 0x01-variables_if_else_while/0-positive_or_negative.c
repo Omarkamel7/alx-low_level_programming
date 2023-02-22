e9e85f1957e97ce2dc933e3e47077570dcb445a6
omar kamel <Omarkamel7.com>
2023-02-22 06:02:35 -0800

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,8 +20,9 @@ int main(void)
                 printf("%d is zero\n",n);
         }
         else if (n < 0)
-        {
-                printf("%d is negative\n".n);
-        }
-        return (0);
+	{
+                printf("%d is negative\n",n);
+	}
+        
+	return (0);
 }
