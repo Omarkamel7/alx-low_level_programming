a39c7113e926d2376ddfd4cb77a22ab4e7334eb9
omar kamel <Omarkamel7.com>
2023-02-22 23:43:36 -0800

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet_x10(void) 10 times the alphabet, in lowercase
+ * followed by a new line.
+ */
+void print_alphabet_x10(void)
+{
+char ch;
+int i;
+ch = 'a';
+for (ch = 'a'; ch <= 'j'; ch++ );
+   _putchar(ch);
+_putchar("\n");
+}
