99cabe8f23b40fac605569a805edee5c2ccf559a
omar kamel <Omarkamel7.com>
2023-02-28 11:54:41 -0800

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+int nn = o;
+while (*s != '0')
+{
+nn++;
+s++
+}
+return (nn);
+}
